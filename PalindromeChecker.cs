73e429d06aaea84b07319f1934f5c2200d59403a
Ancelia Mathias Patrao <55922585+anceliaa06@users.noreply.github.com>
2025-06-26 22:18:16 +0530

    Update PalindromeChecker.cs

--- PalindromeChecker.cs
+++ PalindromeChecker.cs
@@ -4,18 +4,20 @@ namespace PalindromeChecker
     {
         static void Main(string[] args)
         {
-            Console.Write("ENTER THE NUMBER TO CHECK IF PALINDROME:");
+            Console.Write("ENTER THE NUMBER TO CHECK IF PALINDROME: ");
             int number = Convert.ToInt32(Console.ReadLine());
-            int remainder = 0;
-            int sum = 0;
-            int temp = number;
-            while (remainder > 0)
+            int remainder; //for last digit
+            int sum = 0; //reversed
+            int temp = number; //to hold the original number
+
+            while (temp > 0)
             {
-                remainder = temp % 10;
-                sum = (sum * 10) + remainder;
-                temp = temp / 10;
+                remainder = temp % 10; //extract last digit
+                sum = (sum * 10) + remainder; //reverses number
+                temp = temp / 10;  //removes last digit from temp
             }
-            if (temp == sum)
+
+            if (number == sum)
             {
                 Console.WriteLine("The number is a palindrome.");
             }
